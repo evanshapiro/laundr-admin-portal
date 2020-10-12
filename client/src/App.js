5f1633f4e7f271b70c9d7dfc2f5420dbef1a19ff
Hannah Soliman <hannahsolismile@gmail.com>
2020-10-12 12:11:51 -0400

    Login screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import {observer} from 'mobx-react';
+import UserStore from './stores/UserStore';
+import LoginForm from './components/LoginForm';
+import SubmitButton from './components/SubmitButton';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  async componentDidMount(){
+
+    try{
+
+      let res = await fetch('/isLoggedIn', {
+        method: 'post',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      let result = await res.json();
+
+      if (result && result.success){
+        UserStore.loading = false;
+        UserStore.isLoggedIn = true;
+        //UserStore.username = result.username;
+      }
+
+      else{
+        UserStore.loading = false;
+        UserStore.isLoggedIn = false;
+      }
+    
+    }
+
+    catch(e){
+      UserStore.loading = false;
+      UserStore.isLoggedIn = false;
+    }
+  } 
+  // create logout function here
+
+  render(){
+
+    if(UserStore.loading){
+      return (
+        <div className="app">
+          <div className= "container">
+            Loading, please wait...  
+          </div>
+        </div>
+      );
+    }
+
+    else{
+
+      if(UserStore.isLoggedIn){
+        return (
+          <div className="app">
+            <div className= "container">
+              Welcome 
+            </div>
+          </div>
+        );
+      }
+      return (
+        <div className="app">
+          <div className='container'>
+            <LoginForm />
+          </div>
+        </div>
+      );
+    }
+  }
+  
 }
 
-export default App;
+export default observer(App);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-const express = require('./config/express.js')
- 
+const express = require('express');
+var mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.DB_URI || require('./config/config.js').db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() =>
+{
+    console.log(`Successfully connected to MongoDB.`)
+});
 // Use env port or default
 const port = process.env.PORT || 5000;
 
-const app = express.init()
+const app = express();
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
 
