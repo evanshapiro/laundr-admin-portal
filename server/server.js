1dc6f726c95066512b154abbe21f66f4acd4755b
evanshapiro <esxyz@icloud.com>
2020-10-28 15:28:36 -0400

    heroku setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
+const path = require('path');
 const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
 var mongoose = require('mongoose');
 
+// Use env port or default
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
 
 mongoose.connect(process.env.DB_URI || require('./config/config.js').db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() =>
 {
     console.log(`Successfully connected to MongoDB.`)
 });
-// Use env port or default
-const port = process.env.PORT || 5000;
 
-const app = express();
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
 
