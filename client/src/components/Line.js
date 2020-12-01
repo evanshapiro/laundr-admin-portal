855e79cf58d580a0dba4b027a1d3c1b5fd3bdaa0
tiffanyscott <tiffanyscott18@hotmail.com>
2020-11-30 23:07:48 -0500

    semi-responsive graph

--- client/src/components/Line.js
+++ client/src/components/Line.js
@@ -7,55 +7,66 @@ import {
   ChartLabel,
   HorizontalGridLines,
   VerticalGridLines,
-  LineSeries
+  LineSeries,
+  FlexibleXYPlot,
 } from "react-vis";
 
 export default class Line extends React.Component {
   render() {
-    const Line = LineSeries;
+    const LineData = LineSeries;
 
     return (
-      <div>
-        <XYPlot width={900} height={250}>
-          <HorizontalGridLines />
-          <VerticalGridLines />
-          <XAxis />
-          <YAxis />
-          <ChartLabel
-            text="X Axis"
-            className="alt-x-label"
-            includeMargin={false}
-            xPercent={0.025}
-            yPercent={1.01}
-          />
-          <ChartLabel
-            text="Y Axis"
-            className="alt-y-label"
-            includeMargin={false}
-            xPercent={0.06}
-            yPercent={0.06}
-            style={{
-              transform: "rotate(-90)",
-              textAnchor: "end"
-            }}
-          />
-          <Line
-            data={[
-              { x: 1, y: 3 },
-              { x: 2, y: 5 },
-              { x: 3, y: 15 },
-              { x: 4, y: 12 },
-              { x: 5, y: 14 },
-              { x: 6, y: 11 },
-              { x: 7, y: 15 },
-              { x: 8, y: 12 },
-              { x: 9, y: 16 },
-              { x: 10, y: 5 },
-              { x: 11, y: 15 },
-              { x: 12, y: 20 }
-            ]}
-          />
-        </XYPlot>
+      <div class="rv-xy-plot" style={{ flex: 1 }}>
+        <div
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex",
+            width: "95%",
+            height: "40vh",
+          }}
+        >
+          <FlexibleXYPlot>
+            <HorizontalGridLines />
+            <VerticalGridLines />
+            <XAxis />
+            <YAxis />
+            <ChartLabel
+              text="X Axis"
+              className="alt-x-label"
+              includeMargin={false}
+              xPercent={0.025}
+              yPercent={1.01}
+            />
+            <ChartLabel
+              text="Y Axis"
+              className="alt-y-label"
+              includeMargin={false}
+              xPercent={0.06}
+              yPercent={0.06}
+              style={{
+                transform: "rotate(-90)",
+                textAnchor: "end",
+              }}
+            />
+            <LineData
+              data={[
+                { x: 1, y: 3 },
+                { x: 2, y: 5 },
+                { x: 3, y: 15 },
+                { x: 4, y: 12 },
+                { x: 5, y: 14 },
+                { x: 6, y: 11 },
+                { x: 7, y: 15 },
+                { x: 8, y: 12 },
+                { x: 9, y: 16 },
+                { x: 10, y: 5 },
+                { x: 11, y: 15 },
+                { x: 12, y: 20 },
+              ]}
+            />
+          </FlexibleXYPlot>
+        </div>
       </div>
     );
   }
